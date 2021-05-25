4bdf4a66cd89db93932c11668b131ef8c2daf3d3
HuuPhu9601 <nguyenhuuphu01hy@gmail.com>
2021-05-25 19:21:30 +0700

    Add project files.

---
+++ WebLQ/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebLQ.Models;
+using WebLQ.Service;
+namespace WebLQ.Controllers
+{
+    public class HomeController : Controller
+    {
+        AutoChatService auto = new AutoChatService();
+        public IActionResult Index()
+        {
+            List<BinhLuan> binhLuans = new List<BinhLuan>();
+            auto.ReadFile(binhLuans);
+            foreach (var item in binhLuans)
+            {
+                ViewBag.Conment = item.Name + " " + item.Value;
+                break;
+            }
+            return View();
+        }
+
+        public IActionResult MyanmaIndex()
+        {
+            return View();
+        }
+
+        public IActionResult ThaiLangueIndex()
+        {
+            return View();
+        }
+        public IActionResult PhilipLangueIndex()
+        {
+            return View();
+        }
+    }
+}
